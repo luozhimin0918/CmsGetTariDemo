5a0f43368bc304fb20073d8f0d5c3ebbdc6fa68b
luozhimin <838111691@qq.com>
2018-06-22 15:34:19 +0800

    Initial commit

---
+++ tariff/src/main/java/com/example/yinlian/tariff/index/Utills.java
@@ -0,0 +1,32 @@
+package com.example.yinlian.tariff.index;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+
+import java.util.List;
+
+/**
+ * Created by Luozhimin on 2018-06-21.9:42
+ */
+public class Utills {
+
+    /**
+     * 获取当前应用程序的包名
+     * @param context 上下文对象
+     * @return 返回包名
+     */
+    public static String getAppProcessName(Context context) {
+        //当前应用pid
+        int pid = android.os.Process.myPid();
+        //任务管理类
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        //遍历所有应用
+        List<ActivityManager.RunningAppProcessInfo> infos = manager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo info : infos) {
+            if (info.pid == pid)//得到当前应用
+                return info.processName;//返回包名
+        }
+        return "";
+    }
+}
