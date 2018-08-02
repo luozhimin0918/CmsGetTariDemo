d653a05ee9ac8e5c628d055c3d87e92b2ed78b7b
luozhimin <838111691@qq.com>
2018-08-02 17:51:06 +0800

    ":umsTari"222222

--- umsTari/src/main/java/com/ums/asus/umspaydemo/MainActivity.java
+++ umsTari/src/main/java/com/ums/asus/umspaydemo/MainActivity.java
@@ -3,11 +3,29 @@ package com.ums.asus.umspaydemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.yinlian.tariff.index.ApiManager;
+import com.example.yinlian.tariff.model.ReqDetailJson;
+import com.socks.library.KLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ApiManager apiManager=ApiManager.getInstance(getApplicationContext());
+        ReqDetailJson reqDetailJson=new ReqDetailJson();
+        reqDetailJson.setTariffDescList("");
+        apiManager.getTariffInfo("4637b348589c493fbac91b6b5f0029f1",reqDetailJson,new ApiManager.RespCallBack() {
+            @Override
+            public void onResponse(String jsonRespString) {
+                KLog.json("ApiMa",jsonRespString);
+            }
+        }, new ApiManager.RespErrorCallBack() {
+            @Override
+            public void onError(String errorStr) {
+                KLog.d("ApiMa",errorStr);
+            }
+        });
     }
 }
